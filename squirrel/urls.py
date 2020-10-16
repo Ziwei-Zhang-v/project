ff4a5a5466c68880ed6604ab237e256a18421073
Penny Peng <pennypeng@Pennys-MBP.lan>
2020-10-15 22:32:43 -0400

    update urls

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('sightings/', views.sightings, name='sightings'),
     path('map/', views.index,name='index'),
     path('sightings/add/', views.add,name='add'),
-    path('sightings/<unique-squirrel-id>/', views.update,name='update'),
+    path('sightings/<unique_squirrel_id>/', views.update,name='update'),
     path('sightings/stats/', views.stats,name='stats'),
 ]
