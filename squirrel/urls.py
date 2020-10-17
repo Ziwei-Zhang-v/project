34dad310d9de63b854003220ef04ddfe9131ffb9
Ziwei Zhang <zz2764@columbia.edu>
2020-10-17 02:38:51 +0000

    Modified urls

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -10,8 +10,8 @@ App_name = 'squirrel'
 urlpatterns = [
     path('', views.homepage),
     path('sightings/', views.sightings, name='sightings'),
-    path('map/', views.index,name='index'),
-    path('sightings/add/', views.add,name='add'),
-    path('sightings/<unique_squirrel_id>/', views.update,name='update'),
-    path('sightings/stats/', views.stats,name='stats'),
+    path('map/', views.index, name='index'),
+    path('sightings/add/', views.add, name='add'),
+    path('sightings/stats/', views.stats, name='stats'),
+    path('sightings/<unique_squirrel_id>/', views.update, name='update'),
 ]
